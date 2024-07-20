be735ceb804b9045d65e5730bba5002301a09007
Joshua Sorensen <joshuasorensen1@mail.weber.edu>
2024-07-20 16:42:35 -0600

    just added paths to the sessions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
-import React from "react";
- 
-// We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
-// We import all the components we need in our app
+import React, { useState } from 'react';
+import './styles.css';
+import Highscore from './Highscores';
 import RecordList from "./components/recordList";
 
 // import all the sessions we need, using session_set as maybe our start page???
 import Session_Set from "./components/session_set.js";
 import Session_Delete from "./components/session_delete.js";
 
- 
+
+const words = ["react", "hangman", "javascript", "frontend"];
+
+const getRandomWord = () => {
+  return words[Math.floor(Math.random() * words.length)];
+};
+
 const App = () => {
  return (
    <div>
     <h1>Hello World</h1>
      <Routes>
        <Route exact path="/" element={<RecordList />} />
-       <Route path="/session_set" element={<Session_Set/>} />
-       <Route path="/session_delete" element={<Session_Delete/>} />
      </Routes>
    </div>
  );
 };
- 
+
 export default App;
