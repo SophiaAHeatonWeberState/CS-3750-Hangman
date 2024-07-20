9fdcea0b7db72cbb8d7c1a77f06cd6519a26ba6e
carson <cartymoore@gmail.com>
2024-07-20 16:08:10 -0600

    Merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,9 @@
-/*import React from "react";
- 
-// We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
-// We import all the components we need in our app
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-
- 
-const App = () => {
- return (
-   <div>
-    <h1>Hello World</h1>
-     <Routes>
-       <Route exact path="/" element={<RecordList />} />
-     </Routes>
-   </div>
- );
-};
- 
-export default App;*/
-
 import React, { useState } from 'react';
 import './styles.css';
 import Highscore from './Highscores';
+import RecordList from "./components/recordList";
+
 
 const words = ["react", "hangman", "javascript", "frontend"];
 
